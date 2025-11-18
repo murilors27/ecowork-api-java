8ad84ef8080e89b4de0aba95c63009d1e39dfd5b
murilors27 <murilorsantos27@gmail.com>
2025-11-18 12:30:58 -0300

    chore: setup inicial do projeto EcoWork

---
+++ src/main/java/com/ecowork/EcoWorkApplication.java
@@ -0,0 +1,13 @@
+package com.ecowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecowork/EcoWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecowork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
